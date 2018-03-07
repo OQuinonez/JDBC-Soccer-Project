b1d15c45478ec91b65473a54cdc49e683dd3b21e
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-07 11:45:19 -0600

    Joined the Players Table and the Teams table so I could get the name of the team a specefic player is on

--- SPlayerRepositroy.java
+++ SPlayerRepositroy.java
@@ -6,19 +6,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SPlayerRepositroy {
-    public static SoccerPlayer MakePlayer(String TeamName){
+    public static void DisplayPlayer() {
         try {
             Connection con = Connect.LoadDB();
-            // SQL SELCECT STATEMENT BELOW!!!!!
-            PreparedStatement statement = con.prepareStatement("SELECT FName" +
-                    "LName" +
-                    "TeamName WHERE TeamID = TeamID IN Teams");
+            PreparedStatement statement = con.prepareStatement("SELECT Players.FName, " +
+                    "Players.LName, " + "Teams.TeamName " +
+                    "FROM Players JOIN Teams ON Players.TeamID = Teams.TeamID ");
             ResultSet resultSet = statement.executeQuery();
             resultSet.next();
-            return new SoccerPlayer(resultSet.getString("FName"), resultSet.getString("LName"), resultSet.getString("team"));
-        } catch (SQLException exception){
+            System.out.println(resultSet.getString("FName") + " " + resultSet.getString("LName") + " " + resultSet.getString("TeamName"));
+//            return new SoccerPlayer(resultSet.getString("FName"), resultSet.getString("LName"), resultSet.getString("team"));
+        } catch (SQLException exception) {
             System.out.println(exception.getMessage());
-            return null;
+//            return null;
         }
     }
 }
