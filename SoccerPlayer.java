a00fcf3b00c9a8c2188908720e60136baf4a22c8
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-07 08:47:48 -0600

    Made the Soccer Player Class which right now it only states what the soccer player class will have

---
+++ SoccerPlayer.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class SoccerPlayer {
+    public String FName;
+    public String LName;
+    public String team;
+
+    public SoccerPlayer (String FName, String LName, String team){
+        this.FName = FName;
+        this.LName = LName;
+        this.team = team;
+    }
+}
