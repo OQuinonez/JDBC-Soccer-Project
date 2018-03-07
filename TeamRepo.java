9485f2499281b9bf17a9f3934dba980d939e6454
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-07 11:47:08 -0600

    Created the TeamRepo File and Connected it to the DB so I could get the team name and the number of players

--- TeamRepo.java
+++ TeamRepo.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class TeamRepo {
+    public static void DisplayTeam(){
+        try {
+            Connection con = Connect.LoadDB();
+            PreparedStatement statement = con.prepareStatement("SELECT TeamName, " +
+                    "NumOfPlayers FROM Teams");
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            System.out.println(resultSet.getString("TeamName") + " " + resultSet.getInt("numofplayers"));
+        } catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
 }
