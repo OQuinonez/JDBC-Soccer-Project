d5c8effe9e56c6cb6ac196a6e4809139e41911ac
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-08 15:15:19 -0600

    Added Helper Methods to have a clean main file, and imported the scanner

--- Main.java
+++ Main.java
@@ -2,15 +2,30 @@ package com.company;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
+    public static void Intro(){
+        System.out.println("Hello, Here is the Liga Mexicana Table\n");
+        System.out.println("Team Name" + "\t" + "Ranking");
+        TeamRepo.DisplayTeam();
+    }
+    public static void ChooseTeam(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Which team would you like to look at?");
+        System.out.println("(Choose the Ranking Number)");
+        String TeamChosen = scan.nextLine();
+    }
 
     public static void main(String[] args) throws SQLException {
+        Scanner scan = new Scanner(System.in);
 	// write your code here
 //        System.out.println("Hello World");
 //        Connect.LoadDB();
 //        try {
 //            Connect con = new Connect();con.LoadDB();
 //        }
+        Intro();
+//        SPlayerRepositroy.DisplayPlayer();
     }
 }
