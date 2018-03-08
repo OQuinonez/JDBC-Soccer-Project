8a7898e95b9a29867f47b823f44036cf447d87f6
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-08 15:16:53 -0600

    Fixed DisplayTeam method to make it display all of the teams

--- TeamRepo.java
+++ TeamRepo.java
@@ -9,11 +9,11 @@ public class TeamRepo {
     public static void DisplayTeam(){
         try {
             Connection con = Connect.LoadDB();
-            PreparedStatement statement = con.prepareStatement("SELECT TeamName, " +
-                    "NumOfPlayers FROM Teams");
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM Teams");
             ResultSet resultSet = statement.executeQuery();
-            resultSet.next();
-            System.out.println(resultSet.getString("TeamName") + " " + resultSet.getInt("numofplayers"));
+            while(resultSet.next()) {
+                System.out.println(resultSet.getString("TeamName") + "\t\t\t" + resultSet.getInt("Ranking"));
+            }
         } catch (SQLException exception){
             System.out.println(exception.getMessage());
         }
