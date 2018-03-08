ec2567a20d8bcad9912c54232022192a3315d00a
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-08 15:16:18 -0600

    Fixed a bug which let me display all of the players at the same time

--- SPlayerRepositroy.java
+++ SPlayerRepositroy.java
@@ -9,12 +9,13 @@ public class SPlayerRepositroy {
     public static void DisplayPlayer() {
         try {
             Connection con = Connect.LoadDB();
-            PreparedStatement statement = con.prepareStatement("SELECT Players.FName, " +
-                    "Players.LName, " + "Teams.TeamName " +
+            PreparedStatement statement = con.prepareStatement("SELECT *" +
+//                    "Players.LName, " + "Teams.TeamName " +
                     "FROM Players JOIN Teams ON Players.TeamID = Teams.TeamID ");
             ResultSet resultSet = statement.executeQuery();
-            resultSet.next();
-            System.out.println(resultSet.getString("FName") + " " + resultSet.getString("LName") + " " + resultSet.getString("TeamName"));
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("FName") + " " + resultSet.getString("LName") + " " + resultSet.getString("TeamName"));
+            }
 //            return new SoccerPlayer(resultSet.getString("FName"), resultSet.getString("LName"), resultSet.getString("team"));
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
