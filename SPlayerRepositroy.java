0ee31b3330f7f1179a7aba7623b058f1d0c1f7c0
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-08 21:42:51 -0600

    Added a method that displayes a players statistics, and another one that displays the players information

--- SPlayerRepositroy.java
+++ SPlayerRepositroy.java
@@ -6,20 +6,47 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SPlayerRepositroy {
-    public static void DisplayPlayer() {
+    public static void DisplayPlayer(Integer TeamRank) {
         try {
             Connection con = Connect.LoadDB();
-            PreparedStatement statement = con.prepareStatement("SELECT *" +
-//                    "Players.LName, " + "Teams.TeamName " +
-                    "FROM Players JOIN Teams ON Players.TeamID = Teams.TeamID ");
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM Players WHERE TeamID=" + TeamRank );// +
+//                    "Players.LName, " + "Teams.TeamName WHERE TEAMS.TEAMNAME=AGUILAS " +
+//                    "FROM Players JOIN Teams ON Players.TeamID = Teams.TeamID ");
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("FName") + " " + resultSet.getString("LName") + " " + resultSet.getString("TeamName"));
+                System.out.println(resultSet.getString("FName") + " " + resultSet.getString("LName") + "\t\t"
+                + resultSet.getInt("PlayerID")); //+ resultSet.getString("TeamName"));
             }
-//            return new SoccerPlayer(resultSet.getString("FName"), resultSet.getString("LName"), resultSet.getString("team"));
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
-//            return null;
+        }
+    }
+
+    public static void DisplayPLayerStatistics(Integer PlayerID){
+        try {
+            Connection con = Connect.LoadDB();
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM PlayerStatistics WHERE PlayerID=" + PlayerID);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("Goals") + "\t\t" + resultSet.getInt("Assist") + "\t\t" +
+                        resultSet.getInt("YCards") + "\t\t\t"+ resultSet.getInt("RCards"));
+            }
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public static void DisplayPLayerInfo(Integer PlayerID){
+        try {
+            Connection con = Connect.LoadDB();
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM PlayerInfo WHERE PlayerID=" + PlayerID);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("Pos") + "\t\t" + resultSet.getInt("Age") + "\t\t" +
+                        resultSet.getString("Nationality"));
+            }
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
         }
     }
 }
